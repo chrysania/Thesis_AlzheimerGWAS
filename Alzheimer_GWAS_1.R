d6ac8f990800f9ed66ff468aac18127b7388f216
chrysania <87006370+chrysania@users.noreply.github.com>
2023-02-09 13:54:28 +0700

    Add files via upload

--- Alzheimer_GWAS_1.R
+++ Alzheimer_GWAS_1.R
@@ -12,7 +12,7 @@ system("plink --file adgwas --make-bed --out alzheimer")
 
 
 
-##### Quality control calculations #####
+##### Quality control #####
 
 # Main QC: genotyping, hwe, maf
 system("plink --bfile alzheimer --geno 0.1 --hwe 0.05 --maf 0.01 --make-bed --out alzheimerQC")
@@ -24,12 +24,12 @@ system("plink --bfile alzheimerQC --freq --out alzheimerQC1")
 system("plink --bfile alzheimerQC --check-sex --out alzheimerQC2")
 
 # QC3: IBD calculation
-system("plink --bfile alzheimerQC --genome rel-check --out azlheimerQC3")
+system("plink --bfile alzheimerQC --genome rel-check --out alzheimerQC3")
 
 # QC4: PCA calculation
 system("plink --bfile alzheimerQC --pca --out alzheimer_QC4")
 
-########################################
+###########################
 
 
 
@@ -37,5 +37,10 @@ system("plink --bfile alzheimerQC --pca --out alzheimer_QC4")
 # note: coversion to .vcf is for importing to hail
 
 system("plink --bfile alzheimerQC --recode vcf --out alzheimer_vcf")
+
+# extra QC to remove out of bounds SNPs (GRCH37)
+system("plink --bfile alzheimerQC --exclude filterout5.txt --make-bed --out alzheimer_Hail")
+
+system("plink --bfile alzheimer_Hail --recode vcf --out alzheimer_vcf_2")
 
 ##########################################
