672fa48d2744143a219ee1e81007bf7a3b8ae725
chrysania <87006370+chrysania@users.noreply.github.com>
2023-02-27 15:11:22 +0700

    Update Alzheimer_GWAS_3.R

--- Alzheimer_GWAS_3.R
+++ Alzheimer_GWAS_3.R
@@ -4,6 +4,7 @@
 
 setwd("C:/Users/chrys/Desktop/plink19")
 library(dplyr)
+library(qqman)
 
 ##### Simple association #####
 
@@ -13,5 +14,10 @@ system("plink --bfile alzheimerQC --assoc --adjust --out alzheimerSA")
 df_SA <- read.csv("C:/Users/chrys/Desktop/plink19/alzheimerSA.assoc", sep="")
 SA_SNP <- df_SA %>% filter(P <= 0.00000005) %>% select(SNP, CHR, BP, A1, A2, P)
 View(SA_SNP)
+
+# Manhattan plot & qqplot
+manhattan(df_SA, annotatePval = 0.00000005)
+
+qq(df_SA$P, main = "Q-Q plot of Simple Association p-values")
 
 ##############################
