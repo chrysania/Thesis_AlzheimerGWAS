6c2c64e8cdc9b43851267c9273fa989054102402
chrysania <87006370+chrysania@users.noreply.github.com>
2023-03-21 17:06:21 +0700

    Add files via upload

--- Alzheimer_GWAS_4.R
+++ Alzheimer_GWAS_4.R
@@ -1,6 +1,6 @@
 # Alzheimer GWAS Thesis Project - Chrysania Lim
 # R file 4 of 5: Logistic Regression
-# status: in progress
+# status: completed
 
 setwd("C:/Users/chrys/Desktop/plink19")
 library(dplyr)
@@ -8,25 +8,13 @@ library(qqman)
 
 ##### Logistic regression #####
 
-# all pheno setting
-system("plink --bfile alzheimerQC --logistic --all-pheno --pheno residuals.pheno --covar covariates_plink.covar --out alzheimerLogReg1")
-
-# tests all 3 covariates: diagnosis, age, APOE
-system("plink --bfile alzheimerQC --logistic --covar covariates_plink.covar --out alzheimerLogReg2")
-
-# test all default covariates
-system("plink --bfile alzheimerQC --logistic --covar samples.covar --out alzheimerLogReg3")
-
-# no covariates
-system("plink --bfile alzheimerQC --logistic --out alzheimerLogReg4")
-
-## note: only no covar succeeded
-
+# Diagnosis phenotype
+system("plink --bfile alzheimerQC --logistic --pheno pheno_diagnosis.pheno --allow-no-sex --out alzheimerLogReg")
 
 # select significant SNPs (no covar)
-alzheimerLogReg4.assoc <- read.csv("C:/Users/chrys/Desktop/plink19/alzheimerLogReg4.assoc.logistic", sep="")
-df_LogReg <- alzheimerLogReg4.assoc
-LogReg_SNP <- df_LogReg %>% filter(P <= 0.00000005) %>% select(SNP, CHR, BP, TEST, OR, STAT, P)
+alzheimerLogReg.assoc <- read.csv("C:/Users/chrys/Desktop/plink19/alzheimerLogReg.assoc.logistic", sep="")
+df_LogReg <- alzheimerLogReg.assoc
+LogReg_SNP <- df_LogReg %>% filter(P <= 0.00000005) %>% select(SNP, CHR, BP, A1, TEST, OR, STAT, P)
 LogReg_SNP
 
 # Manhattan plot & qqplot
